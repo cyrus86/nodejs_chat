34106a2d4b884a15893c5241c3e09b8fb5e11dcd
cyrus <github@mycyrus.com>
2015-11-29 10:38:31 +0900

    Implement chat history with redis server

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redis = require('redis');
+var client = redis.createClient();
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+var ChatMessage = function(id, nickname, msg) {
+    self = this;
+    self.timestamp = getChatTimestamp();
+    self.id = id;
+    self.nickname = nickname;
+    self.msg = msg;
+}
+var ServerMessage = function(msg) {
+    self = this;
+    self.timestamp = getChatTimestamp();
+    self.msg = msg;
+}
+
+function getChatTimestamp() {
+    var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+    var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
+    return localISOTime.substr(2, 8) + ' ' + localISOTime.substr(11, 8);
+}
 io.on('connection', function(socket) {
-    console.log(socket.conn.id+' '+socket.client.conn.remoteAddress);
-    io.emit('chat message', {id: '', nickname: '<strong class="text-warning">system</strong>', msg: 'new user connected with ID: '+socket.conn.id+ ' from IP: '+ socket.client.conn.remoteAddress });
+    console.log(socket.conn.id + ' ' + socket.client.conn.remoteAddress);
+    client.lrange("chatMessages", 0, -1, function(err, msgs) {
+        // improved native for-loop works perfect with chronological chat history.
+        for (var i = msgs.length - 1; i >= 0; i--) {
+            msg = JSON.parse(msgs[i]);
+            io.emit('chat message', msg);
+        }
+        connectionMessage = new ServerMessage('new user connected with ID: ' + socket.conn.id + ' from IP: ' + socket.client.conn.remoteAddress);
+        io.emit('server message', connectionMessage);
+    });
+
     socket.on('chat message', function(data) {
-        console.log(data);
-        io.emit('chat message', {id: socket.conn.id, nickname: data.nickname,  msg: data.msg });
-        //io.emit('handshake', socket.handshake);
+        chatMessage = new ChatMessage(socket.conn.id, data.nickname, data.msg);
+        console.log(chatMessage);
+        jsonChatMessage = JSON.stringify(chatMessage);
+        client.lpush('chatMessages', jsonChatMessage);
+        io.emit('chat message', chatMessage);
     });
 });
-
 http.listen(3000, function() {
     console.log('listening on *:3000');
 });
